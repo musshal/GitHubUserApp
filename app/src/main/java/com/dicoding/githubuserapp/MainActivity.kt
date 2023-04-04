4852b12a074b2abbc2b0e512a97bf688252b979b
Musthafa Faishal <musthafafaishal@gmail.com>
2023-04-04 20:49:50 +0700

    Create menu setting and search vew

--- app/src/main/java/com/dicoding/githubuserapp/MainActivity.kt
+++ app/src/main/java/com/dicoding/githubuserapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.dicoding.githubuserapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.dicoding.githubuserapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_setting, menu)
+        return true
     }
 }
