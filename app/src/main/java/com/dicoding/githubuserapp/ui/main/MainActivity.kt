f7ec3f79e4666bd96fb811ac5aca585ec8fbeece
Musthafa Faishal <musthafafaishal@gmail.com>
2023-04-23 20:39:04 +0700

    Update: search bar appearance

--- app/src/main/java/com/dicoding/githubuserapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/githubuserapp/ui/main/MainActivity.kt
@@ -12,6 +12,7 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewTreeObserver
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.datastore.core.DataStore
@@ -208,10 +209,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onSubmit(userInput: String) {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
         if (userInput != "") {
             mainViewModel.findUsers(userInput)
+            searchEditText.clearFocus()
+            inputMethodManager.hideSoftInputFromWindow(searchEditText.windowToken, 0) // hide keyboard after search operation
         } else {
             mainViewModel.getUsers()
+            searchEditText.clearFocus()
+            inputMethodManager.hideSoftInputFromWindow(searchEditText.windowToken, 0) // hide keyboard after search operation
         }
     }
 }
